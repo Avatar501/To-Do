ecea45bb882634de5a5758bee1dbe015c48a009a
Tahir Tirar <tahirtirar123@gmail.com>
2024-11-21 11:53:09 +0100

    validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-// import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
-// import Home from './pages/Home';
-// import Login from './pages/Login';
-// import Register from './pages/Register';
-// import Header from './components/Header';
-import { Outlet } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Header from './components/Header';
 
 function App() {
   return (
-    // <Router>
-    //   <Header />
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/login" element={<Login />} />
-    //     <Route path="/register" element={<Register />} />
-    //   </Routes>
-    // </Router>
-    <Outlet/>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
 
-// Create a BrowserRouter instance and pass routes to it.
-const router = createBrowserRouter([
-  {
-    path: '/To-Do/',
-    element: <App/>,
-    children: [
-      { path: '/To-Do/', element: <Home /> },
-      { path: '/To-Do/login', element: <Login /> },
-      { path:'/To-Do/register', element: <Register /> }
-    ]
-  }
-]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
