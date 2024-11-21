f39853e0563d6ec33d91cc129d4cf9a0c2d6c810
Tahir Tirar <tahirtirar123@gmail.com>
2024-11-21 11:28:11 +0100

    sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Header from './components/Header';
+// import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
+// import Home from './pages/Home';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
+// import Header from './components/Header';
+import { Outlet } from "react-router-dom";
+
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Header />
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/register" element={<Register />} />
+    //   </Routes>
+    // </Router>
+    <Outlet/>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
+// Create a BrowserRouter instance and pass routes to it.
+const router = createBrowserRouter([
+  {
+    path: '/vite-react-router/',
+    element: <App/>,
+    children: [
+      { path: '/vite-react-router/', element: <Home /> },
+      { path: '/vite-react-router/login', element: <Login /> },
+      { path:'/vite-react-router/register', element: <Register /> }
+    ]
+  }
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
